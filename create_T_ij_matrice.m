58862da2d50b01af5150474e4c5a90a5135881a4
St4v0 <91466882+St4v0@users.noreply.github.com>
2021-11-12 14:27:33 +0000

    Add files via upload

---
+++ create_T_ij_matrice.m
@@ -0,0 +1,12 @@
+function T_ij = create_T_ij_matrice(a,alpha,d,theta)
+ca = cosd(alpha);
+sa = sind(alpha);
+ct = cosd(theta);
+st = sind(theta);
+
+T_ij = [ct -ca*st sa*st a*ct ;st ca*ct -sa*ct a*st; 0 sa ca d; 0 0 0 1];
+end
+
+
+
+
