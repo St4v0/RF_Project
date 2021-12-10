1b8d0e483e7001e031b48b466068ff39e0858b0e
Francois Wieckowiak <legentil42@gmail.com>
2021-12-10 13:07:04 +0000

    typo atan2

--- inverse_kin.m
+++ inverse_kin.m
@@ -14,7 +14,7 @@ R = rho_prime^2+z_prime^2+L1^2-L2^2;
 sigma = 1;
 gamma = atan2(Q/sqrt(P^2+Q^2),P/sqrt(P^2+Q^2));
 
-theta1 = atan2(x,y);
+theta1 = atan2(y,x);
 theta2 = gamma+sigma*acos(-R/(2*L1*sqrt(rho_prime^2+z_prime^2)));
 theta3 = atan2((z_prime-L1*sin(theta2))/L2,(rho_prime-L1*cos(theta2))/L2)-theta2;
 theta4 = psi-theta2-theta3;
